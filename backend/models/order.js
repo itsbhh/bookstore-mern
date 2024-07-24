0786bf8dac75cb050dfdc8adf3015d0cec43fd46
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-07-24 08:06:56 +0530

    Database Schemas Created

---
+++ backend/models/order.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const order = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "user",
+    },
+    book: {
+      type: mongoose.Types.ObjectId,
+      ref: "books",
+    },
+    status: {
+      type: string,
+      default: "Order Placed",
+      enum: ["Order Placed", "Out for Delivery, Delivered,Canceled"],
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("order", order);
