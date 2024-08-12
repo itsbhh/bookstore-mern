b79654f224eb04b62dc790b83adb7c5e8cf9b851
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-08-12 09:40:31 +0530

    Update order.js

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -31,6 +31,56 @@ router.post("/place-order", authenticateToken, async (req, res) => {
   }
 });
 
+//get order history of particular user
+router.get("/get-order-history", authenticateToken, async (req, res) => {
+  try {
+    const { id } = req.headers;
+    const userData = await User.findById(id).populate({
+      path: "orders",
+      populate: { path: "books" },
+    });
+    const ordersData = userData.orders.reverse();
+    return res.json({
+      status: "Success",
+      data: ordersData,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "An error occured" });
+  }
+});
 
+//get all orders --admin
+router.get("/get-all-orders", authenticateToken, async (req, res) => {
+  try {
+    const userData = await Order.find()
+      .populate({
+        path: "book",
+      })
+      .populate({
+        path: "user",
+      })
+      .sort({ createdAt: -1 });
+    return res.json({
+      status: "Success",
+      data: userData,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "An error occured" });
+  }
+});
+
+//update order --admin
+router.put("/update-status/:id", authenticateToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Order.findByIdAndUpdate(id, { status: req.body.status });
+    return res.json({
+      status: "Success",
+      message: "Status Updated Successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "An error occured" });
+  }
+});
 
 module.exports = router;
