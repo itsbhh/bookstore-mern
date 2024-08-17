daaec1037800ab2ea877f92f20ef8e51987ea109
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-08-17 08:16:04 +0530

    updated Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div> <div class="html-container">
+    <div class="html-section">
+      <div class="html-project">
+        <div class="navigation">
+          <nav>
+            <ul class="nav-type">
+              <li><a href="https://codepen.io/EneergeticTomy/" target="_blank" class="active">Home</a></li>
+              <li><a href="https://codepen.io/TomikaBoy/full/LqMpaE" target="_blank" class="active1">About</a></li>
+              <li><a href="https://codepen.io/" target="_blank" class="active2">Contact</a></li>
+              <li><a href="https://w3schools.com/" target="_blank" class="active3">Support</a></li>
+              <div class="line"></div>
+              <li><a><i class="fa fa-search" onclick="show()">
+                </i></a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <noscript>
+    <div class="first-line">
+      <div class="linear">
+        <div class="line-w5">
+          
+        </div>
+      </div>
+    </div>
+    </noscript>
+<template>
+    <div class="flash-fluid">
+      <div class="flash-time">
+        <div class="flash-GIF">
+          
+        </div>
+      </div>
+     </div>
+</template>
+<noscript>     
+    <div class="cyrcle-center">
+      <div class="radial-cyrcle">
+        
+      </div>
+    </div>
+</noscript>
+  </div>
+<template>   
+  <div class="text-class">
+    <div class="text-container">
+      <div class="text-content-fluid" id="chinese-text" data-text-type="chinese">
+        <div class="chinese-text">設</div>
+        <div class="chinese-text2">計</div>
+        <div class="chinese-text3">和</div>
+        <div class="chinese-text4">開</div>
+        <div class="chinese-text5">發</div>
+      </div>
+    </div>
+  </div>
+</template>  
+  
+  <div class="dropdown" id="anotherFunction">
+   <div class="drop1">HTML</div>
+    <div class="drop2">CSS</div>
+    <div class="drop3">Javascript</div>
+    <div class="drop4">Sass</div>
+  </div>
+</div>
   )
 }
 
