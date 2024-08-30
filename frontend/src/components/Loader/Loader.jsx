9c104964e6157399a5b5d22e891bcd6ad93d3066
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-08-30 08:50:38 +0530

    LOADER

---
+++ frontend/src/components/Loader/Loader.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="hourglassBackground">
+      <div className="hourglassContainer">
+        <div className="hourglassCurves"></div>
+        <div className="hourglassCapTop"></div>
+        <div className="hourglassGlassTop"></div>
+        <div className="hourglassSand"></div>
+        <div className="hourglassSandStream"></div>
+        <div className="hourglassCapBottom"></div>
+        <div className="hourglassGlass"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
