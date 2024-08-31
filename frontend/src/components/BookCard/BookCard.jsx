8a283fed51b52b21d82dc800d7ab0a6a9bee79d9
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-08-31 12:12:23 +0530

    Update BookCard.jsx

---
+++ frontend/src/components/BookCard/BookCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const BookCard = ({ data }) => {
+  return (
+    <>
+      <Link to={`/view-book-details/${data._id}`}>
+        <div className="bg-zinc-800 rounded p-4 flex flex-col">
+          <div className="bg-zinc-900 rounded flex items-center justify-center">
+            <img src={data.url} alt="/" className="h-[25vh]" />
+          </div>
+          <h2 className="mt-4 text-xl text-white font-semibold">{data.title}</h2>
+          <p className="mt-2 text-zinc-400 font-semibold">by {data.author}</p>
+          <p className="mt-2 text-zinc-200 font-semibold text-xl">by $ {data.price}</p>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default BookCard;
