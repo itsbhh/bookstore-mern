aca4a370115bac0a28a0d7e856fa91fec90dac21
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-09-02 09:00:42 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
@@ -9,7 +9,24 @@ import SignUp from "./pages/SignUp";
 import Cart from "./pages/Cart";
 import Profile from "./pages/Profile";
 import ViewBookDetails from "./components/ViewBookDetails/ViewBookDetails";
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "./store/auth";
+import Favourites from "./components/Profile/Favourites";
+import UserOrderHistory from "./components/Profile/UserOrderHistory";
+import Settings from "./components/Profile/Settings";
 const App = () => {
+  const dispatch = useDispatch();
+  const role = useSelector((state) => state.auth.role);
+  useEffect(() => {
+    if (
+      localStorage.getItem("id") &&
+      localStorage.getItem("token") &&
+      localStorage.getItem("role")
+    ) {
+      dispatch(authActions.login());
+      dispatch(authActions.changeRole(localStorage.getItem("role")));
+    }
+  });
   return (
     <div>
       <Navbar />
@@ -17,7 +34,11 @@ const App = () => {
         <Route exact path="/" element={<Home />} />
         <Route path="/all-books" element={<AllBooks />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<Profile />} >
+        <Route index element={<Favourites />}/>
+        <Route path="/profile/orderHistory" element={<UserOrderHistory />}/>
+        <Route path="/profile/settings" element={<Settings />}/>
+        </Route>
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/LogIn" element={<LogIn />} />
         <Route path="/view-book-details/:id" element={<ViewBookDetails />} />
