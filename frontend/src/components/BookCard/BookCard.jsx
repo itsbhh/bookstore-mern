31e9d26f7da9f1f3330ca15309b9b586c3293e1a
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-09-03 09:25:49 +0530

    Update BookCard.jsx

--- frontend/src/components/BookCard/BookCard.jsx
+++ frontend/src/components/BookCard/BookCard.jsx
@@ -1,19 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const BookCard = ({ data }) => {
-  return (
-    <>
+const BookCard = ({ data, favourite }) => {
+    const headers = {
+        id: localStorage.getItem("id"),
+        authorization: `Bearer ${localStorage.getItem("token")}`,
+        bookid: data._id,
+      };
+    const handleRemoveBook = async () => {
+        const response = await axios.put(
+          `http://localhost:1000/api/v1/remove-book-from-favourite`,
+          {},
+          { headers }
+        );
+        alert(response.data.message);
+      };  return (
+    <div className="bg-zinc-800 rounded p-4 flex flex-col">
       <Link to={`/view-book-details/${data._id}`}>
         <div className="bg-zinc-800 rounded p-4 flex flex-col">
           <div className="bg-zinc-900 rounded flex items-center justify-center">
             <img src={data.url} alt="/" className="h-[25vh]" />
           </div>
-          <h2 className="mt-4 text-xl text-white font-semibold">{data.title}</h2>
+          <h2 className="mt-4 text-xl text-white font-semibold">
+            {data.title}
+          </h2>
           <p className="mt-2 text-zinc-400 font-semibold">by {data.author}</p>
-          <p className="mt-2 text-zinc-200 font-semibold text-xl">by $ {data.price}</p>
+          <p className="mt-2 text-zinc-200 font-semibold text-xl">
+            by $ {data.price}
+          </p>
+        
         </div>
       </Link>
-    </>
+      {favourite && (
+            <button
+              className="bg-yellow-50  px-4 py-2 rounded border border-yellow-500 text-yellow-500 mt-4"
+              onClick={handleRemoveBook}
+            >
+              Remove From Favourite
+            </button>
+          )}
+   </div>
   );
 };
 
