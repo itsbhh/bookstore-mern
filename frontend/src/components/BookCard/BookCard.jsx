ac15bf208a068b43b101a0a0e51def5be4bbdf8d
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-09-06 20:25:41 +0530

    Update BookCard.jsx

--- frontend/src/components/BookCard/BookCard.jsx
+++ frontend/src/components/BookCard/BookCard.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 const BookCard = ({ data, favourite }) => {
-    const headers = {
-        id: localStorage.getItem("id"),
-        authorization: `Bearer ${localStorage.getItem("token")}`,
-        bookid: data._id,
-      };
-    const handleRemoveBook = async () => {
-        const response = await axios.put(
-          `http://localhost:1000/api/v1/remove-book-from-favourite`,
-          {},
-          { headers }
-        );
-        alert(response.data.message);
-      };  return (
+  const headers = {
+    id: localStorage.getItem("id"),
+    authorization: `Bearer ${localStorage.getItem("token")}`,
+    bookid: data._id,
+  };
+  const handleRemoveBook = async () => {
+    const response = await axios.put(
+      `http://localhost:1000/api/v1/remove-book-from-favourite`,
+      {},
+      { headers }
+    );
+    alert(response.data.message);
+  };
+  return (
     <div className="bg-zinc-800 rounded p-4 flex flex-col">
       <Link to={`/view-book-details/${data._id}`}>
         <div className="bg-zinc-800 rounded p-4 flex flex-col">
@@ -25,20 +27,19 @@ const BookCard = ({ data, favourite }) => {
           </h2>
           <p className="mt-2 text-zinc-400 font-semibold">by {data.author}</p>
           <p className="mt-2 text-zinc-200 font-semibold text-xl">
-            by $ {data.price}
+            $ {data.price}
           </p>
-        
         </div>
       </Link>
       {favourite && (
-            <button
-              className="bg-yellow-50  px-4 py-2 rounded border border-yellow-500 text-yellow-500 mt-4"
-              onClick={handleRemoveBook}
-            >
-              Remove From Favourite
-            </button>
-          )}
-   </div>
+        <button
+          className="bg-yellow-50  px-4 py-2 rounded border border-yellow-500 text-yellow-500 mt-4"
+          onClick={handleRemoveBook}
+        >
+          Remove From Favourite
+        </button>
+      )}
+    </div>
   );
 };
 
