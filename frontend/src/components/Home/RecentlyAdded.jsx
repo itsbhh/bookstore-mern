58bd266de7fbba32ebe73f141701ba764c8edd2d
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-09-06 20:25:11 +0530

    Update RecentlyAdded.jsx

--- frontend/src/components/Home/RecentlyAdded.jsx
+++ frontend/src/components/Home/RecentlyAdded.jsx
@@ -1,20 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import BookCard from "../BookCard/BookCard";
 import Loader from "../Loader/Loader";
-import { useState } from "react";
-import { useEffect } from "react";
 
 const RecentlyAdded = () => {
-  const [Data, setData] = useState();
+  const [Data, setData] = useState(null);
+
   useEffect(() => {
-    const fetch = async () => {
+    const fetchData = async () => {
       const response = await axios.get(
-        "http://localhost:1000/api/v1//get-recent-books"
+        "http://localhost:1000/api/v1/get-recent-books"
       );
       setData(response.data.data);
     };
-    fetch();
+    fetchData();
   }, []);
 
   return (
@@ -22,17 +21,16 @@ const RecentlyAdded = () => {
       <h4 className="text-3xl text-yellow-100">Recently Added Books</h4>
       {!Data && (
         <div className="flex items-center justify-center my-8">
-          {" "}
-          <Loader />{" "}
+          <Loader />
         </div>
       )}
-      <div className="my-8 grid  grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-8"></div>
-      {Data &&
-        Data.map((items, i) => (
-          <div key={i}>
-            <BookCard data={items} />
-          </div>
-        ))}
+      {Data && (
+        <div className="my-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {Data.map((item, i) => (
+            <BookCard key={i} data={item} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
