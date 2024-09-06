8f9913b5ae6b98409b8c182aa3274bc4168356c8
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-09-06 21:12:08 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../../models/user");
+const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { authenticateToken } = require("./userAuth");
