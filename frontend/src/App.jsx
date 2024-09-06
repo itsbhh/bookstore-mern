5dc0d3ea0c71ca6a88dd834d9ab0cc75110379f6
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-09-06 20:24:53 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,10 @@ import { authActions } from "./store/auth";
 import Favourites from "./components/Profile/Favourites";
 import UserOrderHistory from "./components/Profile/UserOrderHistory";
 import Settings from "./components/Profile/Settings";
+import AllOrders from "./pages/AllOrders";
+import AddBook from "./pages/AddBook";
+import UpdateBook from "./pages/UpdateBook";
+import AboutUs from "./pages/AboutUs";
 const App = () => {
   const dispatch = useDispatch();
   const role = useSelector((state) => state.auth.role);
@@ -33,14 +37,23 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/all-books" element={<AllBooks />} />
+        <Route path="/about-us" element={<AboutUs />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/profile" element={<Profile />} >
-        <Route index element={<Favourites />}/>
-        <Route path="/profile/orderHistory" element={<UserOrderHistory />}/>
-        <Route path="/profile/settings" element={<Settings />}/>
+        <Route path="/profile" element={<Profile />}>
+          {role === "user" ? (
+            <Route index element={<Favourites />} />
+          ) : (
+            <Route index element={<AllOrders />} />
+          )}
+          {role === "admin" && (
+            <Route path="/profile/addBook" element={<AddBook />} />
+          )}
+          <Route path="/profile/orderHistory" element={<UserOrderHistory />} />
+          <Route path="/profile/settings" element={<Settings />} />
         </Route>
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/LogIn" element={<LogIn />} />
+        <Route path="/UpdateBook/:id" element={<UpdateBook />} />
         <Route path="/view-book-details/:id" element={<ViewBookDetails />} />
       </Routes>
       <Footer />
